edae9aa0a7bb5586fdd606d92a20543bc651963d
manuelromana <mr.romanamanuel@gmail.com>
2021-11-30 22:52:54 +0100

    creating component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
+import { CatalogueComponent } from './catalogue/catalogue.component';
+import { MenuComponent } from './menu/menu.component';
+import { ShippingComponent } from './shipping/shipping.component';
+import { FaqComponent } from './faq/faq.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AboutComponent,
+    CatalogueComponent,
+    MenuComponent,
+    ShippingComponent,
+    FaqComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
